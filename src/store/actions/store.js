aa05b56aa0715307eb0c9a44c7a1f915165da79e
TheSon <thesonk7@gmail.com>
2022-07-11 22:04:51 +0700

    fix: deploy server

--- src/store/actions/store.js
+++ src/store/actions/store.js
@@ -1,12 +1,5 @@
-import { reducers } from "../reducers/index";
-import { createStore,applyMiddleware,compose } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
+import { reducers } from "../reducers/index";
 
-
-export const store = createStore(
-    reducers,
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-)
+export const store = createStore(reducers, compose(applyMiddleware(thunk)));
